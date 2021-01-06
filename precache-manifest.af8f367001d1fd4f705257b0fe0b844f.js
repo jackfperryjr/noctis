0e28111a54bc72dcf31ccb62db74fe56081a4179
jackfperryjr <jackfperryjr@gmail.com>
2021-01-06 23:55:26 +0000

    gh-pages

---
+++ precache-manifest.af8f367001d1fd4f705257b0fe0b844f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7691f30bf76d427f2feb0a110945420b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e40b9a51712de2770fec",
+    "url": "/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "c25768990b0cb66cfaa9",
+    "url": "/static/css/main.9ffc557c.chunk.css"
+  },
+  {
+    "revision": "e40b9a51712de2770fec",
+    "url": "/static/js/2.63f15189.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/static/js/2.63f15189.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c25768990b0cb66cfaa9",
+    "url": "/static/js/main.9fcabe38.chunk.js"
+  },
+  {
+    "revision": "294de3d3da66299eb7f8",
+    "url": "/static/js/runtime-main.c3dedbdd.js"
+  },
+  {
+    "revision": "44b122e7da65ae036a3f2e64b903e3c4",
+    "url": "/static/media/logo512.44b122e7.png"
+  }
+]);
